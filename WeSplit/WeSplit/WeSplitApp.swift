cef3f569e40d797481c432884e586e8f490894d7
dipti <dipti.yadav@outlook.com>
2023-06-20 21:34:10 -0400

    Starting WeSplit project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Dipti Yadav on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
