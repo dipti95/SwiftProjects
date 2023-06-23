1e2f54b3ac17c268fa7b9e875f95ebcab77e1fd3
dipti <dipti.yadav@outlook.com>
2023-06-23 09:37:08 -0400

    learn about views and modifiers

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Dipti Yadav on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
