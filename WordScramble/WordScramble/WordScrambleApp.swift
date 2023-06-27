5163e549c9cd0f327167b49d12dba4191f212255
dipti <dipti.yadav@outlook.com>
2023-06-27 09:23:57 -0400

    WordScamble start

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Dipti Yadav on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
