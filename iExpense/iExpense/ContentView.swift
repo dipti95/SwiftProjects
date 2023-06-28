cf2c392e112df66326f1218b4a0058a1903d432e
dipti <dipti.yadav@outlook.com>
2023-06-28 13:34:53 -0400

    iExpense start

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Dipti Yadav on 6/28/23.
+//
+
+import SwiftUI
+
+class User: ObservableObject {
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
+}
+
+struct ContentView: View {
+    @StateObject private var user = User()
+
+    var body: some View {
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName).")
+
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Dipti Yadav on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
