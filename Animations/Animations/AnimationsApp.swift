cba027d6389a1fea0e9ecd3886c43d3c56877056
dipti <dipti.yadav@outlook.com>
2023-06-28 13:25:49 -0400

    learn about animation in SwiftUI

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Dipti Yadav on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
