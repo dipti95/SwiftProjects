0b90e1b2c6eabd9fc6fae107507ce8a52157640d
dipti <dipti.yadav@outlook.com>
2023-06-29 15:08:51 -0400

    moonshot

---
+++ Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Dipti Yadav on 6/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("Example")
+            .resizable()
+            .frame(width: 300, height: 300)
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
