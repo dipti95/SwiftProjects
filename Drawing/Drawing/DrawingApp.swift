18ddca92a7ca3a9a96bc4bb1af022d889b2d42f7
dipti <dipti.yadav@outlook.com>
2023-06-30 23:33:54 -0400

    Learned about InsettableShape

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Dipti Yadav on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
